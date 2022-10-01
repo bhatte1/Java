69f48148117ccd52ea5fef35fcf899850a08a41a
bhatte1 <vinayak.bhatte@gmail.com>
2022-10-01 16:36:15 -0400

    updated one

--- Introduction/src/whileDemo.java
+++ Introduction/src/whileDemo.java
@@ -1,8 +1,19 @@
+import java.lang.reflect.Array;
 
 public class whileDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		for (int i = 0; i<4; i ++) {
+			for(int j = 1; j<11;j++) {
+				
+				System.out.println(j);
+			}
+		}
+		
+		
 		
 		
 
