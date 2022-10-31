406bc3e74fdb10521dec295718aa3a3de5b6a63e
bhatte1 <vinayak.bhatte@gmail.com>
2022-10-30 20:13:04 -0400

    updated one

--- Introduction/src/hashSetExample.java
+++ Introduction/src/hashSetExample.java
@@ -6,6 +6,7 @@ public class hashSetExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Hash set does not gurantee order
 		HashSet<String> hs = new HashSet<String>();
 		
 		hs.add("Vianayk");
