fd2aaad8065ae63a794052f6a440417c64506406
bhatte1 <vinayak.bhatte@gmail.com>
2023-01-14 17:55:42 -0500

    hashmap

--- practice1/src/practice1/hashMap.java
+++ practice1/src/practice1/hashMap.java
@@ -1,10 +1,36 @@
 package practice1;
 
+import java.util.HashMap;
+
 public class hashMap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		hashMap hm = new hashMap();
+		
+		hm.getHashMap();
+		
 
 	}
 
+	
+	public void getHashMap() {
+		
+		String str = "abcddbsabe";
+		
+		HashMap<String, Integer> hMap = new HashMap<>();
+		
+		for(int i = 0; i <= str.length(); i++) {
+			
+			hMap.put("Vinayak",i);
+			
+			System.out.println(hMap);
+		}
+		
+		
+		
+		
+		
+	}
 }
