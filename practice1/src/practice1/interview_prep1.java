e66918cf5583280be35510f4a2a64e6f2914e6a2
bhatte1 <vinayak.bhatte@gmail.com>
2023-01-25 23:10:21 -0500

    hashMap

--- practice1/src/practice1/interview_prep1.java
+++ practice1/src/practice1/interview_prep1.java
@@ -2,6 +2,9 @@ package practice1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import javax.sound.midi.SysexMessage;
 
 public class interview_prep1 {
 
@@ -16,14 +19,48 @@ public class interview_prep1 {
 	
 	public void getList() {
 		
-		String [] question1 = {"1", "2", "2", "2", "3", "3", "4", "4", "1", "1"};		
+		/*String [] question1 = {"1", "2", "2", "2", "3", "3", "4", "4", "1", "1"};	
+		
+		String s = "abced";
+		
 		ArrayList<String> list = new ArrayList<String>(Arrays.asList(question1));
-		list.addAll(Arrays.asList(question1));
+	
+		System.out.println(list); */
+		
+		Integer [] lists = {1,2, 2, 2, 3, 3, 4, 4, 1, 1};
+		
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2,2,2,3,3,4,4,1,1));
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		int temp = 1;
+		
+		int l = list.size();
+		
+		for(int i = 1 ; i <l ; i++) {
+			
+			
+			if(list.get(i) == list.get(i-1) && i== l-1) {
+				temp += 1;
+				list2.add(temp);
+				list2.add(list.get(i-1));
+			}
+				else if(list.get(i) == list.get(i-1))	{
+				
+					temp += 1;
+				}	
+					else {
+						
+						list2.add(temp);
+						list2.add(list.get(i-1)); 
+						temp = 1;
+					}
+							
+			}
 	
 		System.out.println(list);
-		
-		//ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2,2,2,3,3,4,4,1,1));
-		
-	}
+		System.out.println(list2);
 
 }
+	
+}
+
+
