7328c6e2fa53a9bc0c78d9628ebb53521b5292a4
bhatte1 <vinayak.bhatte@gmail.com>
2023-01-31 22:56:40 -0500

    Lecture1

---
+++ Jan2023JavaSessions/src/javasessions/DataTypesConcept.java
@@ -0,0 +1,21 @@
+package javasessions;
+
+public class DataTypesConcept {
+
+	public static void main(String[] args) {
+		String v = "vinayak";
+		Integer k = 10;
+		
+		
+		
+		for(byte i = 0; i <= v.length()-1; i++) {
+			char c = v.charAt(i);
+			System.out.println(c);
+			System.out.println();
+			//syso
+			
+			System.out.println(k);
+		}
+	}
+
+}
