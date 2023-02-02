0215137b47a387dd576cdf87cc23ff0e85de9fc9
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-01 21:58:58 -0500

    Lecture2

---
+++ Jan2023JavaSessions/src/javasessions/StringConcatenation.java
@@ -0,0 +1,27 @@
+package javasessions;
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char c = 'B';
+		
+		char z = 'z';
+		
+		int a  = 100;
+		
+		String name = "Vinayak";
+		
+		System.out.println(name+" "+c);
+		
+		System.out.println(a + c + z);
+		
+		System.out.println('x'+'y');
+		
+		System.out.println(9.0 % 2.0);
+		
+
+	}
+
+}
