b14fe61b25ef36b6f458404932782f1936e1fd3b
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-04 21:42:43 -0500

    assignement1

---
+++ JavaAssignemts/src/java2023assignemts/FirstAssignement.java
@@ -0,0 +1,25 @@
+package java2023assignemts;
+
+public class FirstAssignement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		System.out.println("Vinayak");
+		System.out.println(74+36);
+		System.out.println((double)((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+		System.out.println("Hello Selenium" + " t");
+		char d = 'd';
+		System.out.println(d+3);
+		System.out.println(3.9*3.9);
+
+		
+		int i = 11;
+		
+		i = i++ + ++i;
+		
+		System.out.println(i);
+	}
+
+}
