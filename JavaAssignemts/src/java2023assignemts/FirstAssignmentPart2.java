2c7c3a2b675d5f2d59aa4feb08a5a37331ef8864
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-04 22:00:23 -0500

    assignement1

--- JavaAssignemts/src/java2023assignemts/FirstAssignmentPart2.java
+++ JavaAssignemts/src/java2023assignemts/FirstAssignmentPart2.java
@@ -4,17 +4,9 @@ public class FirstAssignmentPart2 {
 	
 	public static void main(String [] args) {
 		
-		int i=1, j=2, k=3;
+		int a=1, b=2;
 		
-		int m = i-- - j-- - k--;
-
-		System.out.println("i="+i);
-
-		System.out.println("j="+j);
-
-		System.out.println("k="+k);
-
-		System.out.println("m="+m);
+		System.out.println(--b - ++a + ++b - --a);
 	
 	}
 
