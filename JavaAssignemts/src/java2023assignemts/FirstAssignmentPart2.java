4c5800f2c1d63f5c5f5783fbad11c6ba60443b96
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-07 06:38:37 -0500

    assignement2

--- JavaAssignemts/src/java2023assignemts/FirstAssignmentPart2.java
+++ JavaAssignemts/src/java2023assignemts/FirstAssignmentPart2.java
@@ -7,6 +7,21 @@ public class FirstAssignmentPart2 {
 		double d = 1.5, D = 2.5;
 
 		System.out.println(d++ + ++D);
+		
+		
+		final int i = 1;
+		
+		int a = 'A';
+		
+		char b = 'B';
+		
+		System.out.println(++a);
+		
+		
+		boolean test = true;
+		if(!test) {
+			System.out.println("Vinayak");
+		}
 	
 	}
 
---
+++ JavaAssignemts/src/java2023assignemts/SwitchAssignement.java
@@ -0,0 +1,45 @@
+package java2023assignemts;
+
+public class SwitchAssignement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char vowel = 'a';
+		
+		switch (vowel) {
+		case 'a':
+			System.out.println("The letter is vowel");
+			
+			break;
+			
+		case 'e':
+			System.out.println("The letter is vowel");
+			
+			break;
+			
+		case 'i':
+			System.out.println("The letter is vowel");
+			
+			break;
+		case 'o':
+			System.out.println("The letter is vowel");
+			
+			break;
+			
+		case 'u':
+			System.out.println("The letter is vowel");
+			
+			break;
+			
+		default:
+			System.out.println("The letter is consonant");
+			
+			break;
+		}
+		
+		
+
+	}
+
+}
