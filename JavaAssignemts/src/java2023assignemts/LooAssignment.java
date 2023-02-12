878774c02237e393eedcbfd34017e78ed3dd632a
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-11 21:11:40 -0500

    Assignement2

---
+++ JavaAssignemts/src/java2023assignemts/LooAssignment.java
@@ -0,0 +1,20 @@
+package java2023assignemts;
+
+public class LooAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+
+		do {
+			System.out.println(i);
+			
+		} while (i>0); {
+			i--;
+		}	
+
+		
+	}
+
+}
