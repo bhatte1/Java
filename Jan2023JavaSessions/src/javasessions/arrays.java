bb8ea5b4c8a13847d4eff2fb978d941bd61b56e4
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-13 22:25:50 -0500

    Feb13

---
+++ Jan2023JavaSessions/src/javasessions/arrays.java
@@ -0,0 +1,17 @@
+package javasessions;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a[] = new int[3];
+		
+		a[4] = 200;
+		System.out.println(a[4]);
+		
+
+	}
+
+}
