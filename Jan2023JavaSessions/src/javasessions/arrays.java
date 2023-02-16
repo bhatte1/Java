8c048217b6147b8ca4a50e6a732539ae1536dc7e
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-15 20:23:23 -0500

    Feb14

--- Jan2023JavaSessions/src/javasessions/arrays.java
+++ Jan2023JavaSessions/src/javasessions/arrays.java
@@ -20,6 +20,42 @@ public class arrays {
 			System.out.println(o);
 		}
 		
+		String browsers[] = {"chrome", "firefox", "IE", "Edge", "Safari"};
+		for(int k=0; k<browsers.length; k++) {
+			System.out.println(browsers[k]);
+				if(browsers[k].equals("IE")) {
+					System.out.println("this is deprecated");
+				}
+				
+				
+				switch (browsers[k]) {
+				case "chrome": {
+					
+					System.out.println("Google");
+					break;
+				}
+				case "firefox":{
+					System.out.println("Mozilla");
+					break;
+				}
+				//case "IE":{
+					
+					//System.out.println("Microsoft");
+					//break;
+				//}
+				
+				case "Edge":{
+					System.out.println("Microsoft");
+					break;
+				}
+				case "Safari":{
+					System.out.println("Apple");
+					break;
+				}
+	
+				}
+		}
+		
 
 	}
 
