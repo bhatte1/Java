3fe60b2ded11821dea0bad3632e3b39277dfbfdb
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-19 14:14:08 -0500

    Feb17

--- Jan2023JavaSessions/src/javasessions/ObjectUnderstanding.java
+++ Jan2023JavaSessions/src/javasessions/ObjectUnderstanding.java
@@ -21,12 +21,32 @@ public class ObjectUnderstanding {
 		ObjectUnderstanding ou2 = new ObjectUnderstanding();
 		
 		ou2.empNameString = "Akash";
-		ou2.Age = 31;
+		ou2.Age = 17;
 		ou2.MaleFemale = 'M';
 		
-		ou2 =ou1;
 		
 		System.out.println("The current Employee is " + ou2.empNameString + " "+ou2.Age+ " "+ou2.MaleFemale);
+		
+		
+		ObjectUnderstanding ou3 = new ObjectUnderstanding();
+		
+		ou3.empNameString = "Vinayak";
+		ou3.Age = 31;
+		ou3.MaleFemale = 'M';
+		
+		
+		System.out.println("The current Employee is " + ou3.empNameString + " "+ou3.Age+ " "+ou3.MaleFemale);
+		
+		
+		ObjectUnderstanding ou4 = new ObjectUnderstanding();
+		
+		ou4.empNameString = "Meghana";
+		ou4.Age = 25;
+		ou4.MaleFemale = 'F';
+		
+		System.out.println("The current Employee is " + ou4.empNameString + " "+ou4.Age+ " "+ou4.MaleFemale);
+		
+		
 		
 
 	}
