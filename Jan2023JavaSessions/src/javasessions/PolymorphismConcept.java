169096a4277dcbb316fb71a072fe4ee6dd9285d8
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-24 22:14:06 -0500

    polymorphism

---
+++ Jan2023JavaSessions/src/javasessions/PolymorphismConcept.java
@@ -0,0 +1,37 @@
+package javasessions;
+
+public class PolymorphismConcept {
+	
+	
+	public void search() {
+		
+	}
+	
+	
+	public void search(int i) {
+		System.out.println(i);
+		
+		
+	}
+	
+	public static String search(String s) {
+		return s;
+		
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PolymorphismConcept pc = new PolymorphismConcept();
+		
+		pc.search();
+		pc.search(10);
+		System.out.println(PolymorphismConcept.search("Vinayak"));
+		
+
+	}
+
+}
