93772789346116bc8647d291bf0c984a859f3494
bhatte1 <vinayak.bhatte@gmail.com>
2023-02-27 18:43:31 -0500

    StaticConcept

---
+++ Jan2023JavaSessions/src/javasessions/StaticConcept.java
@@ -0,0 +1,46 @@
+package javasessions;
+
+public class StaticConcept {
+	 
+	String sub;
+	static int price;
+	
+	public void location1() {
+		
+		System.out.println("Horn Beck");
+		
+	}
+	public void location2() {
+		
+		System.out.println("High Street");
+		
+	}
+	
+	public void location2(String part1) {
+		
+		System.out.println(part1);
+		
+	}
+	
+	static public void location3() {
+		
+		System.out.println("Morgantown");
+		
+	}
+	
+	public int price(int a, int b) {
+		
+		return a+b;
+	}
+	
+	static public String subwayName(String type) {
+		return type;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
