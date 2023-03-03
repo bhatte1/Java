5f48381b22d2813cc4a8b446ff1f3dd1588f059a
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-02 19:48:56 -0500

    ConstructorAssignment

---
+++ JavaAssignemts/src/java2023assignemts/Person.java
@@ -0,0 +1,18 @@
+package java2023assignemts;
+
+public class Person {
+
+	String name;
+	int age;
+	char gender;
+	double height;
+
+	public Person(String name, int age, char gender, double height) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.height = height;
+
+	}
+
+}
---
+++ JavaAssignemts/src/java2023assignemts/PersonMain.java
@@ -0,0 +1,18 @@
+package java2023assignemts;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Person person = new Person("Vinayak", 31, 'M', 5.9);
+		
+		System.out.println(person.age);
+		System.out.println(person.gender);
+		System.out.println(person.height);
+		System.out.println(person.name);
+		
+
+	}
+
+}
