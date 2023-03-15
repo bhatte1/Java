1c249757b76b7876dd95fb133403342f74247fdd
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-14 22:44:52 -0400

    TyCatchEpisode

--- Jan2023JavaSessions/src/oopsEncapsulation/EmployeeDetails.java
+++ Jan2023JavaSessions/src/oopsEncapsulation/EmployeeDetails.java
@@ -1,5 +1,7 @@
 package oopsEncapsulation;
 
+import java.util.Arrays;
+
 public class EmployeeDetails {
 
 	public static void main(String[] args) {
@@ -10,7 +12,27 @@ public class EmployeeDetails {
 		
 		employee.getEmployeeData();
 		
+		
+		
+		EmployeeDetails employeeDetails = new EmployeeDetails();
+		
+		employeeDetails.getArrya();
+		
 
+	}
+	
+	
+	
+	public void getArrya() {
+		
+		int a[] = new int[4];
+		
+		a[0] = 45;
+		a[3] = 90;
+		
+		System.out.println(Arrays.toString(a));
+		
+		
 	}
 
 }
