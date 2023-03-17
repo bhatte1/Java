051b92e758c79ffc1c73caa0c1124e7f14e2290e
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-16 23:12:54 -0400

    splitMethiod

---
+++ practice1/src/practice1/BasicPractice.java
@@ -0,0 +1,21 @@
+package practice1;
+
+import java.util.Arrays;
+
+public class BasicPractice {
+
+	public static void main(String[] args) {
+		 
+String st = "Vinay is a good boy";
+
+String[] splitStrings = st.split(" ");
+
+
+System.out.println(Arrays.toString(splitStrings));
+
+
+		 
+
+	}
+
+}
