df02287c61484765a368ca1a725a8f57518edde2
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-20 20:12:57 -0400

    strihImmutable

--- Jan2023JavaSessions/src/javasessions/StingImmutable.java
+++ Jan2023JavaSessions/src/javasessions/StingImmutable.java
@@ -5,6 +5,37 @@ public class StingImmutable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		String s = "Selenium";
+		
+		s = s+"Techno";
+		System.out.println(s);
+		
+		String t = s + "Hello";
+		System.out.println(t);
+		System.out.println(s);
+		
+		String st = "Java";
+		System.out.println(st+"Python");
+		System.out.println(st);
+		
+		st = st + "Automation";
+		System.out.println(st);//JavaAutomation
+				
+		String u = "Java";
+		
+		//mutable classes:
+		//StringBuilder(1.5) and StringBuffer(1.0):
+		StringBuilder sb = new StringBuilder("Testing");
+		System.out.println(sb);
+		sb.append("Automation");
+		System.out.println(sb);
+		
+		String pop = "    submit   ";
+		pop = pop.trim();
+		System.out.println(pop);
+		
+		System.out.println(pop.startsWith("s"));
 	}
 
 }
