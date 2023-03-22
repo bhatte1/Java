1944f4838a641ba7511e7d4f83c4dbdfdb08be04
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-21 21:04:36 -0400

    stringMethods

--- Jan2023JavaSessions/src/javasessions/StringMethods.java
+++ Jan2023JavaSessions/src/javasessions/StringMethods.java
@@ -14,7 +14,13 @@ public class StringMethods {
 		System.out.println(s.charAt(stringLength-1));
 		
 		System.out.println(s.indexOf('i'));
+		
+		System.out.println(s.indexOf('i', s.indexOf('i')+1));
 
+		String t = "hello this is a java code";
+		System.out.println(t.toUpperCase());
+		System.out.println(t.toLowerCase());
+		
 	}
 
 }
