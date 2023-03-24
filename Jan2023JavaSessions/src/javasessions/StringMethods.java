ad8467c71b547ca41047a028fb4fb3224b9cf19d
bhatte1 <vinayak.bhatte@gmail.com>
2023-03-23 21:24:35 -0400

    stringMethods

--- Jan2023JavaSessions/src/javasessions/StringMethods.java
+++ Jan2023JavaSessions/src/javasessions/StringMethods.java
@@ -50,6 +50,53 @@ public class StringMethods {
 				System.out.println(d[4]);
 				System.out.println(Arrays.toString(d));
 				
+				
+				//
+				String x = "100";
+				System.out.println(x+20);
+				
+				//string to int:
+				int i = Integer.parseInt(x);//100
+				System.out.println(i+20);//120
+				
+				//string to double:
+				String v = "12.33";
+				System.out.println(v+10);
+				double d1 = Double.parseDouble(v);//12.33
+				System.out.println(d1+10);
+				
+				//
+				//int to String:
+				int k = 1100;
+				System.out.println(k+20);
+				String k1 = String.valueOf(k); //"1100"
+				System.out.println(k1+20);
+				
+				//string to boolean:
+				String flag = "true";
+				boolean bl = Boolean.parseBoolean(flag);//true
+				if(bl) {
+					System.out.println("run my test cases");
+				}
+				
+				
+				//
+				String h = "testing with selenium";
+				String g = "devlopment with selenium";
+				
+				System.out.println(h.startsWith("testing"));
+				System.out.println(g.endsWith("selenium"));
+				
+				//substring:
+				String messg = "your transaction id is 12345";
+				System.out.println(messg.substring(messg.indexOf("is")+2).trim());
+				String trId = messg.substring(messg.indexOf("is")+2).trim();
+				System.out.println(trId);
+				
+				String mg = "your transaction id is 12345";
+				System.out.println(mg.substring(0, 9));//0-8
+				System.out.println(mg.substring(mg.indexOf("is")+2, mg.length()).trim());
+				
 	}
 
 }
