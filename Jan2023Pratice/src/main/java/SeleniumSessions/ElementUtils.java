7dceb566e890b54f7e56c72a7c4f2cdd6584cc67
bhatte1 <vinayak.bhatte@gmail.com>
2023-04-04 15:10:41 -0400

    Changes

--- Jan2023Pratice/src/main/java/SeleniumSessions/ElementUtils.java
+++ Jan2023Pratice/src/main/java/SeleniumSessions/ElementUtils.java
@@ -1,6 +1,8 @@
 package SeleniumSessions;
 
 import java.sql.Driver;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +10,7 @@ import org.openqa.selenium.WebElement;
 
 public class ElementUtils {
 	private WebDriver driver;
-	
-	
+
 	public ElementUtils(WebDriver driver) {
 		this.driver = driver;
 	}
@@ -21,6 +22,29 @@ public class ElementUtils {
 	
 	public WebElement getElement(By locator) {
 		return driver.findElement(locator);
+		
+	}
+	
+	public List<WebElement> totalTag(By locator) {
+		
+		return driver.findElements(locator);
+		
+	}
+	
+	public List<String> getTextElements(By locator) {
+		
+		List<String> st = new ArrayList<String>();
+		
+		List<WebElement> atag = totalTag(locator);
+		
+		for(WebElement e: atag) {
+			
+			String getText = e.getText();
+			st.add(getText);
+			
+		}
+
+		return st;
 		
 	}
 	
--- Jan2023Pratice/src/main/java/SeleniumSessions/Project2.java
+++ Jan2023Pratice/src/main/java/SeleniumSessions/Project2.java
@@ -18,46 +18,21 @@ public class Project2 {
 		
 		BrowserUtils bu = new BrowserUtils();
 		
+		
 		driver =  bu.browserName("chrome");
 		
 		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
 		
-		//By locator = By.tagName("a");
+		ElementUtils eu = new ElementUtils(driver);
 		
 		By locator = By.className("list-group-item");
-		
-		
-		
-		List<String> getEleTextList = getTextElements(locator);
+
+		List<String> getEleTextList = eu.getTextElements(locator);
 		
 		System.out.println(getEleTextList);
 
 		
 
-	}
-	
-	public static List<WebElement> totalTag(By locator) {
-		
-		return driver.findElements(locator);
-		
-	}
-	
-	public static List<String> getTextElements(By locator) {
-		
-		List<String> st = new ArrayList<String>();
-		
-		List<WebElement> atag = totalTag(locator);
-		
-		for(WebElement e: atag) {
-			
-			String getText = e.getText();
-			st.add(getText);
-			
-		}
-			
-			
-		return st;
-		
 	}
 
 }
