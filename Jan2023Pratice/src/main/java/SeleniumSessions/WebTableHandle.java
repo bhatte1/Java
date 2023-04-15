24f08f0c3247c797e1985a34b0173a39051baf2a
bhatte1 <vinayak.bhatte@gmail.com>
2023-04-14 23:36:32 -0400

    Changes

--- Jan2023Pratice/src/main/java/SeleniumSessions/WebTableHandle.java
+++ Jan2023Pratice/src/main/java/SeleniumSessions/WebTableHandle.java
@@ -10,8 +10,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import net.bytebuddy.build.AndroidDescriptor;
+
 public class WebTableHandle {
-	
+
 	static WebDriver driver;
 
 	public static void main(String[] args) throws InterruptedException {
@@ -24,35 +26,35 @@ public class WebTableHandle {
 //		
 //		
 //		driver.findElement(By.xpath("//a[text()='John.Smith']/parent::td/preceding-sibling::td/input")).click();
-		
+
 		driver = new ChromeDriver();
 		driver.get("https://classic.crmpro.com/");
 		Thread.sleep(4000);
-		
+
 		driver.findElement(By.name("username")).sendKeys("newautomation");
 		driver.findElement(By.name("password")).sendKeys("Selenium@12345");
 		driver.findElement(By.xpath("//input[@value='Login']")).click();
-		
+
 		driver.switchTo().frame("mainpanel");
 		driver.findElement(By.xpath("//a[text()='Contacts']")).click();
-		
-		
-	List<WebElement> detailsElements =	driver.findElements(By.xpath("//a[text()='Ali khan']/parent::td/following-sibling::td"));
-	
-	List<String> details = new ArrayList<String>();
-	
-	
-	for(WebElement e:detailsElements) {
-		String element = e.getText();		
-		details.add(element);
-	}
-	
-	System.out.println(details);
-		
-		
-		
-		
-		
+
+		List<WebElement> detailsElements = driver
+				.findElements(By.xpath("//a[text()='Ali khan']/parent::td/following-sibling::td"));
+
+		List<String> details = new ArrayList<String>();
+
+		for (WebElement e : detailsElements) {
+			String element = e.getText();
+			if (element.isBlank() || element.contains("|")) {
+				System.out.println("Blank or ||");
+			} else {
+				details.add(element.trim());
+			}
+
+		}
+
+		System.out.println(details);
+
 	}
 
 }
