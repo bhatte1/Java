47c0f340c0aad798b1f937de4aa9adaad76bd110
bhatte1 <vinayak.bhatte@gmail.com>
2023-05-16 22:53:41 -0400

    HashMap

---
+++ Jan2023JavaSessions/src/javasessions/VinBioData.java
@@ -0,0 +1,21 @@
+package javasessions;
+
+import java.util.HashMap;
+
+public class VinBioData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HashMap<String, String> vinayakBioDataMap = new HashMap<String,String>();
+		
+		
+		vinayakBioDataMap.put("Name", "Vinayak");
+		
+		
+		
+		System.out.println(vinayakBioDataMap);
+		
+	}
+
+}
