93f64053e27824e598179f2fafaa0b866d4a5bc6
bhatte1 <vinayak.bhatte@gmail.com>
2023-06-10 20:06:28 -0400

    arrayList

--- Jan2023JavaSessions/src/javasessions/StringMethods.java
+++ Jan2023JavaSessions/src/javasessions/StringMethods.java
@@ -96,6 +96,7 @@ public class StringMethods {
 				String mg = "your transaction id is 12345";
 				System.out.println(mg.substring(0, 9));//0-8
 				System.out.println(mg.substring(mg.indexOf("is")+2, mg.length()).trim());
+				System.out.println(mg.startsWith("your"));
 				
 	}
 
